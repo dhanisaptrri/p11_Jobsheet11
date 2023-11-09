5473261826295db84a2f4562837c39cb7a5b9a0d
dhanisaptrri <shadhanisa2@gmail.com>
2023-11-09 21:45:59 +0700

    modisikasi 2.4

--- Quiz07.java
+++ Quiz07.java
@@ -16,10 +16,19 @@ public class Quiz07 {
                 System.out.print("Tebak angka (1-10): ");
                 int answer = scan.nextInt();
                 scan.nextLine();
+
+                if(answer < number){
+                    System.out.println("Yah tebakan ini terlalu kecil, ayo coba lagi!");
+                } else if(answer > number){
+                    System.out.println("Yah tebakan ini telalu besar, ayo coba lagi!");
+                } else{
+                    System.out.println("YEAYY! Tebakan ini sesuai!");
+                }
+                
                 success = (answer == number);
             }while(!success);
             System.out.print("Apakah anda ingin mengulang permainan (Y/y)?");
             menu = scan.nextLine().charAt(0);
-        }while (menu=='Y' || menu =='Y');
+        }while (menu=='Y' || menu =='y');
     }
 }
