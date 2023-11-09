6267d6f4156098e55caa23ff1b09a273f0b509c6
dhanisaptrri <shadhanisa2@gmail.com>
2023-11-10 05:16:32 +0700

    2.5

--- NestedLoop_2341720212.java
+++ NestedLoop_2341720212.java
@@ -12,12 +12,13 @@ public class NestedLoop_2341720212 {
             System.out.println("Kota ke-" + i);
             for (int j = 0; j < temps[0].length; j++) {  
                 System.out.print("Hari ke-" + (j + 1) + ": ");  
-                temps[i][j] = scan.nextDouble();
+            temps[i][j] = scan.nextDouble();
             }
             System.out.println(); 
         }
         for (int i = 0; i < temps.length; i++) {
-            System.out.print("Kota ke-" + (i + 1) + ": ");  for (int j = 0; j < temps[0].length; j++) {  
+            System.out.print("Kota ke-" + (i + 1) + ": ");  
+            for (int j = 0; j < temps[0].length; j++) {  
                 System.out.print(temps[i][j] + " ");
             }
             System.out.println();
