04e6d6aa3551e8604129ee663092896a2977e5a7
dhanisaptrri <shadhanisa2@gmail.com>
2023-11-09 19:09:47 +0700

    first commit

---
+++ Perulangan07.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Perulangan07{
+    
+    public static void main (String[] args){
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Masukan nilai N = ");
+        int N = scan.nextInt();
+
+        for(int i=1; i<=N; i++){
+            System.out.print("*");
+
+        }
+    }
+}
